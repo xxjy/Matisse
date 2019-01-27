d36de728d98f6e8dde748403531709dd8fc9e172
xxjy <xxjy.zzz@gmail.com>
2019-01-27 18:38:06 +0800

    custom

---
+++ matisse/src/main/java/com/zhihu/matisse/listener/OnFinishListener.java
@@ -0,0 +1,8 @@
+package com.zhihu.matisse.listener;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public interface OnFinishListener {
+    boolean onFinish(Activity activity, Intent data);
+}
